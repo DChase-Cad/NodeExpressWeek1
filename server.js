967cb04e890fef22a288e1cfef9f216134b32d83
Dane Chase <danechase@gmail.com>
2021-07-24 11:23:26 -0500

    Week 1 Workshop

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
-const campsiteRouter=require('./routes/campsiteRouter');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,7 +10,19 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
-app.use('/campsites/',campsiteRouter)
+
+//routes
+app.use('/campsites/', campsiteRouter)
+app.use('/promotions/', promotionRouter)
+app.use('/partners/', partnerRouter)
+
+//for html pages in public folder
+app.use(express.static(__dirname + '/public'));
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
 
 
 app.listen(port, hostname, () => {
